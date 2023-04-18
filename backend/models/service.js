16b65aeabf756e1e1f3c8f6d9bdab8e418da556e
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-04-18 17:26:23 -0300

    Completando o services

--- backend/models/service.js
+++ backend/models/service.js
@@ -2,21 +2,31 @@ const mongoose = require('mongoose')
 
 const { schema } = mongoose
 
-const serviceSchema = new schema({
-    name: {
-        type:String,
-        required: true
+const serviceSchema = new schema(
+    {
+        name: {
+            type:String,
+            required: true
+        },
+        description: {
+            type:String,
+            required: true
+        },
+        price: {
+            type:Number,
+            required: true
+        },
+        image: {
+            type:String,
+            required: true
+        }
     },
-    description: {
-        type:String,
-        required: true
-    },
-    price: {
-        type:Number,
-        required: true
-    },
-    image: {
-        type:String,
-        required: true
-    }
-})
+    { timestamp: true }
+)
+
+const Service = mongoose.model('service', serviceSchema)
+
+module.exports = {
+    Service,
+    serviceSchema
+}
