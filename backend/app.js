c67d5689370419a7fcea8a711ab145c54a41c7c6
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-04-19 15:12:16 -0300

    Criando services e routes centralizado

--- backend/app.js
+++ backend/app.js
@@ -9,4 +9,8 @@ const conn = require('./db/conn')
 
 conn()
 
+const routes = require('./routes/router')
+
+app.use('/api', routes)
+
 app.listen(3000, () => console.log('Servidor 3000 on!PAI DA ON PORRA!'))
